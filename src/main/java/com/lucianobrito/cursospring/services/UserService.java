37c79ae7d10fcf652334ff155a3068d4c7274b35
Luciano Brito <lucianobrito.dev@gmail.com>
2021-10-18 00:48:21 -0300

    Service layer, component registration

---
+++ src/main/java/com/lucianobrito/cursospring/services/UserService.java
@@ -0,0 +1,28 @@
+package com.lucianobrito.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucianobrito.cursospring.entities.User;
+import com.lucianobrito.cursospring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
