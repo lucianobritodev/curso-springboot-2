92dc1674d9599d08d5cb204b92bba542f7109645
lucianobritodev <lucianobrito.dev@gmail.com>
2021-10-30 17:59:55 -0300

    User update

--- src/main/java/com/lucianobrito/cursospring/services/UserService.java
+++ src/main/java/com/lucianobrito/cursospring/services/UserService.java
@@ -33,5 +33,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		@SuppressWarnings("deprecation")
+		User entity = repository.getOne(id);
+		updateData(entity, user);
+		return repository.save(entity);
+		
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
 
 }
