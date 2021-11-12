3781e025c0b8602b210b5f0a11ebacafdb1d87e5
lucianobritodev <lucianobrito.dev@gmail.com>
2021-11-12 15:07:36 -0300

    Exception handling - delete

--- src/main/java/com/lucianobrito/cursospring/services/UserService.java
+++ src/main/java/com/lucianobrito/cursospring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.lucianobrito.cursospring.entities.User;
 import com.lucianobrito.cursospring.repositories.UserRepository;
+import com.lucianobrito.cursospring.services.exceptions.DatabaseException;
 import com.lucianobrito.cursospring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User user) {
