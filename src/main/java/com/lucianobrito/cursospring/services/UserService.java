2f6721a5ab8b4e84362dcd5a25ef458d3f8d4bc3
lucianobritodev <lucianobrito.dev@gmail.com>
2021-11-12 15:15:05 -0300

    Exception handling - update

--- src/main/java/com/lucianobrito/cursospring/services/UserService.java
+++ src/main/java/com/lucianobrito/cursospring/services/UserService.java
@@ -3,6 +3,8 @@ package com.lucianobrito.cursospring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,11 +47,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
-		@SuppressWarnings("deprecation")
-		User entity = repository.getOne(id);
-		updateData(entity, user);
-		return repository.save(entity);
-		
+		try {
+			@SuppressWarnings("deprecation")
+			User entity = repository.getOne(id);
+			updateData(entity, user);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
